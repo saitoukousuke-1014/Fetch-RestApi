9d821d88af8e538d64a888229db86b0da01a485e
kousuke <test>
2022-07-06 18:01:10 +0900

    end

--- src/ApiFetch.tsx
+++ src/ApiFetch.tsx
@@ -29,9 +29,6 @@ const ApiFetch = () => {
                     posts.map(post => <li key={post.id}>{post.title}</li>)
                 }
             </ul>
-            
-            
-            
         </div>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ApiFetch from './ApiFetch';
 
 
 const root = ReactDOM.createRoot(
