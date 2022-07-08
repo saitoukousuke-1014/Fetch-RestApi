bc8699c4317d375f7b50c952b0b0a4351db7ed58
kousuke <test>
2022-07-08 19:40:57 +0900

    体裁整えました

--- src/ApiFetch.tsx
+++ src/ApiFetch.tsx
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
-
+//import List from './List'
 
 const ApiFetch = () => {
 
@@ -12,19 +12,22 @@ const ApiFetch = () => {
     const [posts, setPosts] = useState<postContents[]>([]);
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts', {method: 'GET'})
-        .then(res => res.json())
-        .then(data => {
-            setPosts(data)
-            console.log(data)
-        })
-    },[])
+        fetch('https://jsonplaceholder.typicode.com/posts', { method: 'GET' })
+            .then(res => res.json())
+            .then(data => {
+                setPosts(data)
+                console.log(data)
+            })
+    }, [])
 
     return (
         <div>
-            <ul>
+            <ul className="list">
                 {
-                    posts.map(post => <li key={post.id}>{post.title}</li>)
+                    posts.map(post =>
+                        <li key={post.id}>
+                            {post.title}
+                        </li>)
                 }
             </ul>
         </div>
